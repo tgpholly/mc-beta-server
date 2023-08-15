36ef685813966aa1ae41404b8716bf289b8f5684
Holly <onlinegamer748@gmail.com>
2023-08-15 10:32:44 +0100

    Update fileSmasher.ts

--- tooling/fileSmasher.ts
+++ tooling/fileSmasher.ts
@@ -42,10 +42,10 @@ const splitLines = combinedFiles.split("\n");
 const resultLines:Array<string> = new Array<string>();
 
 // Insert allowed imports
-resultLines.push(`import * as dyetty from "dyetty";
-import { createWriteStream, mkdirSync, existsSync, readFileSync, readFile, writeFile, writeFileSync, readdirSync } from "fs";
+resultLines.push(`import { createWriteStream, mkdirSync, existsSync, readFileSync, readFile, writeFile, writeFileSync, readdirSync } from "fs";
 import { deflate, inflate } from "zlib";
 import { createWriter, createReader, IReader, Endian } from "bufferstuff";
+import { Console } from "hsconsole";
 import { Server, Socket } from "net";`);
 
 // Let's process the file to make it usable
