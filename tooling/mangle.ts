0c0d8f7711f0eb756952217391e401168c48daa6
Holly <holly@eusv.net>
2023-11-02 10:40:46 +0000

    Swap smasher out for @vercel/ncc

--- tooling/mangle.ts
+++ tooling/mangle.ts
@@ -2,9 +2,9 @@ import { readFileSync, writeFileSync } from "fs";
 import { minify } from "terser";
 
 (async () => {
-	const mangled = await minify(readFileSync("./build/combined.js").toString(), {
+	const mangled = await minify(readFileSync("./build/index.js").toString(), {
 		mangle: true,
 		toplevel: true,
 	});
-	writeFileSync("./build/index.min.js", `${mangled.code}`);
+	writeFileSync("./build/index.min.js", `${mangled.code?.replaceAll("new Array", "[]")}`);
 })();
